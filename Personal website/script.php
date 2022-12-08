3ec8585ac0f4a661e4ebde30993bd035f6f70586
Ghonixe <116732303+Ghonixe@users.noreply.github.com>
2022-12-08 21:19:23 +0800

    added new files

---
+++ Personal website/script.php
@@ -0,0 +1,9 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+// Use the PHP mail() function or a third-party library to send an email
+// with the user's name, email address, and message
+
+?>
